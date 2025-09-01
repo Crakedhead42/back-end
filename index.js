6ee9fb162cbd4558ba5fb5161eae9877c3439105
Aayush Kumar Prasad <aayushprasad42@gmail.com>
2025-09-02 01:47:07 +0530

    Added app

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+//import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('aayushpdotcom')
+})
+
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login</h1> ')
+})
+
+app.get('/yt',(req,res)=>{
+    res.send('Welcome to Youtube')
+})
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
